5afbfe5ba6e1ab0f0152e6f6941ec5b3d9b4829b
Richard Voelker <richard.voelker.software@gmail.com>
2016-12-06 09:57:28 -0500

    Add Ninject to Web API project.

---
+++ CongoData/CongoData.DataAccess/Concrete/EfCongoRepository.cs
@@ -0,0 +1,11 @@
+﻿using CongoData.DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CongoData.DataAccess.Concrete {
+    public class EfCongoRepository : ICongoRepository {
+    }
+}
